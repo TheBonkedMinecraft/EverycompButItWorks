be5987c619afd28c6eaf9c1844377b0f0f2313f2
Bhavya Singh <bhavyasingh2611@outlook.com>
2023-07-24 21:44:32 +0530

    AAAA

--- src/main/java/org/tbm/server/everycompbutitworks/client/EveryCompatWorkingClient.java
+++ src/main/java/org/tbm/server/everycompbutitworks/client/EveryCompatWorkingClient.java
@@ -1,13 +1,16 @@
 package org.tbm.server.everycompbutitworks.client;
 
-import com.starfish_studios.another_furniture.registry.AFRegistry;
+import com.starfish_studios.another_furniture.block.entity.PlanterBoxBlockEntity;
 import com.starfish_studios.another_furniture.client.renderer.blockentity.*;
 import net.fabricmc.api.ClientModInitializer;
-import org.tbm.server.everycompbutitworks.registry.AFBlockEntityTypes;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
+import org.tbm.server.everycompbutitworks.blocks.ModBlocks;
+
+import static org.tbm.server.everycompbutitworks.registry.AFBlockEntityTypes.createBlockEntityType;
 
 public class EveryCompatWorkingClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        AFRegistry.registerBlockEntityRenderer(AFBlockEntityTypes.PLANTER_BOX, PlanterBoxRenderer::new);
+        BlockEntityRendererRegistry.register(createBlockEntityType(PlanterBoxBlockEntity::new, ModBlocks.FLOWERING_AZALEA_PLANTER), PlanterBoxRenderer::new);
     }
 }
